e81d73394a7e4866abc884b7ad214da2a43162e5
MisunJoo <0987someday@naver.com>
2020-01-26 23:26:57 +0900

    [add] layout inflater

--- mylayoutinflater/src/main/java/com/example/mylayoutinflater/MainActivity.java
+++ mylayoutinflater/src/main/java/com/example/mylayoutinflater/MainActivity.java
@@ -2,17 +2,34 @@ package com.example.mylayoutinflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       container = (FrameLayout) findViewById(R.id.container);
+
         Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // infaltion
+                // activity에서 사용할 수 있는 함수
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true); //xml을 메모리에 객체화
+            }
+        });
 
     }
 }
